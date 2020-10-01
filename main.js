b6e26e498cb1db8b436c7c6f4c17b2ae254c7848
Mohamed Fawzy <mohamed.f.elmorsi@gmail.com>
2020-10-01 21:13:43 +0200

    minify js

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-let navIcon = document.getElementsByClassName("navbar--icon")[0];
-let navMenu = document.getElementsByClassName("navbar--list")[0];
-navIcon.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("Ssss");
-  navMenu.classList.toggle("show");
-});
-VanillaTilt.init(document.querySelector(".trail"), {
-  max: 25,
-  speed: 400,
-});
-AOS.init();
-//It also supports NodeList
-// VanillaTilt.init(document.querySelectorAll(".trail"));
+let navIcon=document.getElementsByClassName("navbar--icon")[0],navMenu=document.getElementsByClassName("navbar--list")[0];navIcon.addEventListener("click",e=>{e.preventDefault(),console.log("Ssss"),navMenu.classList.toggle("show")}),VanillaTilt.init(document.querySelector(".trail"),{max:25,speed:400}),AOS.init();
